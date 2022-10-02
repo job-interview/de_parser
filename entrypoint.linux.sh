6e7260e049eb8f289dac0237fad222b555692380
Kamrul Hasan <k.hasan.rajib@gmail.com>
2022-10-03 02:45:20 +0600

    update(integrations): openapi && aws defaults

--- entrypoint.linux.sh
+++ entrypoint.linux.sh
@@ -1,6 +1,8 @@
-aws dynamodb create-table --table-name GENumbers \
+aws dynamodb create-table --table-name DENumbers \
     --attribute-definitions AttributeName=PK,AttributeType=S AttributeName=SK,AttributeType=S \
     --key-schema AttributeName=PK,KeyType=HASH AttributeName=SK,KeyType=RANGE \
     --endpoint-url http://dynamodb-local:8000 \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
-sam local start-api -p 3000 --host 0.0.0.0 --docker-network aws_backend --container-host 172.17.0.1 --container-host-interface 0.0.0.0
+sam local start-api -p 3000 --host 0.0.0.0 --docker-network aws_backend --container-host 172.17.0.1 \
+    --container-host-interface 0.0.0.0 \
+    --container-env-vars ./.aws.json
